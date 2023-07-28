62ce6916abbb0acb93028bb7a420037aed4bfcea
vahedi <vahedi@rand.org>
2023-07-28 16:44:03 +0000

    Update GPTDocEdit.py

--- GPTDocEdit.py
+++ GPTDocEdit.py
@@ -3,17 +3,17 @@
 import urllib.request, json
 import aspose.words as aw
 import numpy as np
+import glob
 
 
-#Queries ChatGPT with the prompt (includes request and main body)
-#Optional parameters of Temprature, Top_percent, and GPT Class 3/4
 def gptRespond(prompt, t = 1, c = 1, GPT = 3):
+    #Place personal key here in string format
+    KEY = '8febe9879fbe468391391b36ae051eda'
+    #KEY = 'INSERT_KEY_HERE'
+
     try:
         url = "https://apigw.rand.org/openai/RAND/inference/deployments/gpt-35-turbo-v0301-base/chat/completions?api-version=2023-03-15-preview"
-        
-        #Place personal key here in string format
-        #KEY = 'INSERT_KEY_HERE'
-        
+
         if (GPT == 3):
             model = 'gpt-35-turbo'
             key = KEY
@@ -45,38 +45,43 @@ def gptRespond(prompt, t = 1, c = 1, GPT = 3):
     except Exception as e:
         print(e)
 
-        
-
-def gptMergeDoc(inname = 'test',outname = 'merged', temp =2, top_c = .7, GPT =3):
+def gptMergeDoc(temp =2, top_c = .7, GPT =3):
     ##License section
     # editWordLicense = aw.License()
     # editWordLicense.set_license("Aspose.Word.lic")
     
-    #Importing Word Document
-    docToRead = aw.Document(inname + ".docx")
-    
-    #Extract text from Word Document
-    text = np.array([],dtype = object)
-    for paragraph in docToRead.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
-        paragraph = paragraph.as_paragraph()
-        text = np.append(text, paragraph.to_string(aw.SaveFormat.TEXT))
-    text = text[1:-2]
+    #Finding all files with ".doc/x" as an extension
+    for file in glob.glob('*.docx'):
+        print(file)
+        ind = file.find('.doc')
+        #Doc Name
+        inname = file[:ind]
+        #extension Type
+        ext = file[ind:]
+        #Importing First Word Document
+        docToRead = aw.Document(file)
 
-    body = ''
-    for t in text:
-        body = body + t
+        #Extract text from Word Document
+        text = np.array([],dtype = object)
+        for paragraph in docToRead.get_child_nodes(aw.NodeType.PARAGRAPH, True) :    
+            paragraph = paragraph.as_paragraph()
+            text = np.append(text, paragraph.to_string(aw.SaveFormat.TEXT))
+        text = text[1:-2]
 
-    # The target document doc2.
-    doc2 = aw.Document()
-    builder = aw.DocumentBuilder(doc2)
-    request = 'Can you edit the following for grammatical errors?'
-    answer = gptRespond(request + '\n' + body, temp, top_c, GPT)
-    builder.writeln(answer)
+        body = ''
+        for t in text:
+            body = body + t
 
-    # Doc1 now contains changes as revisions.
-    docToRead.compare(doc2, "GPT", dt.today())
-    # save document in local directory
-    name = 'merged'
-    docToRead.save(name + ".docx")
+        # The target document doc2.
+        doc2 = aw.Document()
+        builder = aw.DocumentBuilder(doc2)
+        request = 'Can you edit the following for grammatical errors?'
+        answer = gptRespond(request + '\n' + body, temp, top_c, GPT)
+        builder.writeln(answer)
+
+        # DocToRead now contains changes as revisions.
+        docToRead.compare(doc2, "GPT", dt.today())
+        # save merged document in local directory
+        docToRead.save(inname + '_edited'+ ext)
     
 gptMergeDoc()
